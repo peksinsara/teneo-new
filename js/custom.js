d6b2a392b1ba219c6bdf4ce030295eb1a0763220
Sara Peksin <peksin.sara@gmail.com>
2021-04-21 16:27:12 +0200

    initial layout

---
+++ js/custom.js
@@ -0,0 +1,150 @@
+
+// preloader
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+$(document).ready(function() {
+
+	/* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    // jQuery to collapse the navbar on scroll //
+	$(window).scroll(function() {
+	    if ($(".navbar").offset().top > 50) {
+	        $(".navbar-fixed-top").addClass("top-nav-collapse");
+	    } else {
+	        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+	    }
+	});
+	
+	/* FlexSlider
+	-----------------------------*/ 
+  	$('.flexslider').flexslider({
+      animation: "fade",
+      directionNav: false,
+  	});
+
+  	/* isotope
+  	------------------------------*/
+  	if ( $('.iso-box-wrapper').length > 0 ) { 
+
+	    var $container 	= $('.iso-box-wrapper'), 
+	    	$imgs 		= $('.iso-box img');
+
+	    $container.imagesLoaded(function () {
+
+	    	$container.isotope({
+				layoutMode: 'fitRows',
+				itemSelector: '.iso-box'
+	    	});
+
+	    	$imgs.load(function(){
+	    		$container.isotope('reLayout');
+	    	})
+
+	    });
+
+	    //filter items on button click
+	    $('.filter-wrapper li a').click(function(){
+
+	        var $this = $(this), filterValue = $this.attr('data-filter');
+
+			$container.isotope({ 
+				filter: filterValue,
+				animationOptions: { 
+				    duration: 750, 
+				    easing: 'linear', 
+				    queue: false, 
+				}              	 
+			});	            
+
+			// don't proceed if already selected 
+			if ( $this.hasClass('selected') ) { 
+				return false; 
+			}
+
+			var filter_wrapper = $this.closest('.filter-wrapper');
+			filter_wrapper.find('.selected').removeClass('selected');
+			$this.addClass('selected');
+
+	      return false;
+	    }); 
+	}
+
+	/* wow
+	-------------------------------*/
+	new WOW().init();
+});
+
+
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+
+})(jQuery);
